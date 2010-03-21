ae4fdd8b006c6a37a39931b4ac0695971ea0d41b
Nicolas Kassis <nkassis@galois.(none)>
2010-03-21 16:18:52 -0400

    playing around with colors

--- mniobjweb.rb
+++ mniobjweb.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'lib/mniobject'
+require 'json'
+
 
 object = MNIObject.new("surf_reg_model_both.obj")
 
@@ -8,4 +10,52 @@ object = MNIObject.new("surf_reg_model_both.obj")
 get '/' do
   @object = object
   erb :index
+end
+
+
+get '/model/vertices.json' do
+  vertices = []
+  object.vertices.each do |vector|
+    vector.each do |n|
+      vertices << n
+    end
+  end
+
+  {:vertices => vertices}.to_json
+end
+
+
+get '/model/polygons.json' do
+  polygons = []
+  object.polygons.each do |vector|
+    vector.each do |n|
+      polygons << n
+    end
+  end
+
+
+
+  {:polygons => object}.to_json
+end
+
+
+get '/model/normal_vectors.json' do
+  normal_vectors = []
+  object.normal_vectors.each do |vector|
+    vector.each do |n|
+      normal_vectors << n
+    end
+  end
+        
+  {:normal_vectors => normal_vectors}.to_json
+end
+
+get '/model/colors.json' do
+  colors=[]
+  object.vertices.each do 
+     [0,0,1,0].each do |n|
+       colors << n
+     end
+  end
+  {:colors => colors}.to_json
 end

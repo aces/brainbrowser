fa53238e5b8814b469083a4c952d5fa503395cdb
Nicolas Kassis <nic.kassis@gmail.com>
2011-01-27 10:04:18 -0500

    Delete spurious files

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject brainbrowser "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-		 [aleph "0.1.2-SNAPSHOT"]
-		 [hiccup "0.2.7"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :main brainbrowser.core)
-
--- test/brainbrowser/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns brainbrowser.test.core
-  (:use [brainbrowser.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))

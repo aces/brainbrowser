b840dcbd060d343008f8272361fd149cebf01371
Nicolas Kassis <nic.kassis@gmail.com>
2011-02-12 08:45:03 -0500

    deleting brainloaf folder, no need for that anymore

--- brainloaf/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject brainloaf "0.0.1-SNAPSHOT"
-  :description "Brainloaf support app, including binary manipulation and stuff"
-  :dependencies [[clojure "1.2.0"]
-		 [gloss "0.1.1"]])
--- brainloaf/src/brainloaf/core.clj
+++
@@ -1,1 +0,0 @@
-(ns brainloaf.core)

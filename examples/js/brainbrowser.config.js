218765f151b4124c23aec0ad5cc802e84231a85d
Tarek Sherif <tsherif@gmail.com>
2014-02-26 14:51:34 -0500

    Added missing license headers.

--- examples/js/brainbrowser.config.js
+++ examples/js/brainbrowser.config.js
@@ -16,6 +16,8 @@
 *
 * You should have received a copy of the GNU Affero General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Author: Tarek Sherif  <tsherif@gmail.com> (http://tareksherif.ca/)
 */
 
 (function() {

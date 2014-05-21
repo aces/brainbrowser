13de4696dccc9a77a324b48f6b776993da7ebf45
Tarek Sherif <tsherif@gmail.com>
2014-05-21 05:30:22 -0400

    Example server now sets mime types. Closes #36.

--- examples/surface-viewer-demo.config.js
+++ examples/surface-viewer-demo.config.js
@@ -29,8 +29,8 @@
   BrainBrowser.config.set("worker_dir", "js/brainbrowser/workers/");
 
   // Custom configuration for the Surface Viewer demo app.
-  BrainBrowser.config.set("model_types.freesurferasc.format_hint", 'You can use <a href="http://surfer.nmr.mgh.harvard.edu/fswiki/mris_convert" target="_blank">mris_convert</a> to convert your binary surface files into .asc format.')
-  BrainBrowser.config.set("intensity_data_types.freesurferasc.format_hint", 'You can use <a href="http://surfer.nmr.mgh.harvard.edu/fswiki/mris_convert" target="_blank">mris_convert</a> to convert your binary surface files into .asc format.')
+  BrainBrowser.config.set("model_types.freesurferasc.format_hint", 'You can use <a href="http://surfer.nmr.mgh.harvard.edu/fswiki/mris_convert" target="_blank">mris_convert</a> to convert your binary surface files into .asc format.');
+  BrainBrowser.config.set("intensity_data_types.freesurferasc.format_hint", 'You can use <a href="http://surfer.nmr.mgh.harvard.edu/fswiki/mris_convert" target="_blank">mris_convert</a> to convert your binary surface files into .asc format.');
 
   BrainBrowser.config.set("color_maps", [
     {

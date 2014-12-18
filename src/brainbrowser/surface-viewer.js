a98f0c55306c6df50e1228e27c1d9ad519f91a74
Tarek Sherif <tsherif@gmail.com>
2014-12-18 13:44:28 -0500

    Renamed mniobj intensity to text intensity.

--- src/brainbrowser/surface-viewer.js
+++ src/brainbrowser/surface-viewer.js
@@ -572,7 +572,7 @@
   BrainBrowser.config.set("model_types.freesurferbin.worker", "freesurferbin.worker.js");
   BrainBrowser.config.set("model_types.freesurferbin.binary", true);
   BrainBrowser.config.set("model_types.freesurferasc.worker", "freesurferasc.worker.js");
-  BrainBrowser.config.set("intensity_data_types.mniobj.worker", "mniobj.intensity.worker.js");
+  BrainBrowser.config.set("intensity_data_types.text.worker", "text.intensity.worker.js");
   BrainBrowser.config.set("intensity_data_types.freesurferbin.worker", "freesurferbin.intensity.worker.js");
   BrainBrowser.config.set("intensity_data_types.freesurferbin.binary", true);
   BrainBrowser.config.set("intensity_data_types.freesurferasc.worker", "freesurferasc.intensity.worker.js");

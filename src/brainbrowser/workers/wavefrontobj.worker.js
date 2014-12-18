02e1adca6f5b5fde7f2228d9467e00b7707eb73c
Tarek Sherif <tsherif@gmail.com>
2014-12-18 17:48:18 -0500

    Updated documentation and minor fix to wavefront parsing.

--- src/brainbrowser/workers/wavefrontobj.worker.js
+++ src/brainbrowser/workers/wavefrontobj.worker.js
@@ -40,8 +40,9 @@
     var current_shape;
     var vertices = [];
     var texture_coords = [];
-    var normals = [];
-    var indices, texture_indices, normal_indices;
+    var wavefront_normals = [];
+    var wavefront_shapes = [];
+    var normals, indices, texture_indices, normal_indices;
     var line;
     var line_marker;
     var line_length;
@@ -50,11 +51,12 @@
    
     var result = {};
     var transfer = [];
+    result.shapes = [];
+
 
     data = data.split("\n");
-    result.shapes = [];
     current_shape = {indices: [], texture_indices:[], normal_indices: []};
-    result.shapes.push(current_shape);
+    wavefront_shapes.push(current_shape);
     for(i = 0, count = data.length; i < count; i++) {
       line = data[i].replace(/^\s+/, "").replace(/\s+$/, "").split(/\s+/);
       line_marker = line[0];
@@ -65,12 +67,14 @@
         case "o":
         case "g":
           current_shape = {name: line[1], indices: [], texture_indices:[], normal_indices: []};
-          result.shapes.push(current_shape);
+          wavefront_shapes.push(current_shape);
           break;
         case "v":
-          vertices.push(parseFloat(line[1]));
-          vertices.push(parseFloat(line[2]));
-          vertices.push(parseFloat(line[3]));
+          vertices.push(
+            parseFloat(line[1]),
+            parseFloat(line[2]),
+            parseFloat(line[3])
+          );
           break;
         case "vt":
           for(n = 1; n < line_length; n++){
@@ -78,9 +82,11 @@
           }
           break;
         case "vn":
-          normals.push(parseFloat(line[1]));
-          normals.push(parseFloat(line[2]));
-          normals.push(parseFloat(line[3]));
+          wavefront_normals.push(
+            parseFloat(line[1]),
+            parseFloat(line[2]),
+            parseFloat(line[3])
+          );
           break;
         case "f":
           indices = current_shape.indices;
@@ -118,7 +124,36 @@
     result.vertices = new Float32Array(vertices);
     transfer.push(result.vertices.buffer);
 
-    if (normals.length > 0) {
+    if (wavefront_normals.length > 0) {
+      normals = new Float32Array(vertices.length);
+    }
+
+    wavefront_shapes.forEach(function(wavefront_shape) {
+      var shape = {};
+      var vi, ni;
+
+      shape.indices = new Uint32Array(wavefront_shape.indices);
+      transfer.push(shape.indices.buffer);
+
+      if (wavefront_shape.normal_indices.length > 0) {
+        for (i = 0, count = wavefront_shape.normal_indices.length; i < count; i++) {
+          vi = wavefront_shape.indices[i] * 3;
+          ni = wavefront_shape.normal_indices[i] * 3;
+          normals[vi] = wavefront_normals[ni];
+          normals[vi + 1] = wavefront_normals[ni + 1];
+          normals[vi + 2] = wavefront_normals[ni + 2];
+        }
+      }
+
+      if (wavefront_shape.texture_indices.length > 0) {
+        shape.texture_indices = new Uint32Array(wavefront_shape.texture_indices);
+        transfer.push(shape.texture_indices.buffer);
+      }
+
+      result.shapes.push(shape);
+    });
+
+    if (normals) {
       result.normals = new Float32Array(normals);
       transfer.push(result.normals.buffer);
     }
@@ -127,25 +162,6 @@
       result.texture_coords = new Float32Array(texture_coords);
       transfer.push(result.texture_coords.buffer);
     }
-
-    result.shapes.forEach(function(shape) {
-      shape.indices = new Uint32Array(shape.indices);
-      transfer.push(shape.indices.buffer);
-
-      if (shape.normal_indices.length > 0) {
-        shape.normal_indices = new Uint32Array(shape.normal_indices);
-        transfer.push(shape.normal_indices.buffer);
-      } else {
-        shape.normal_indices = null;
-      }
-
-      if (shape.texture_indices.length > 0) {
-        shape.texture_indices = new Uint32Array(shape.texture_indices);
-        transfer.push(shape.texture_indices.buffer);
-      } else {
-        shape.texture_indices = null;
-      }
-    });
 
     return {
       result: result,

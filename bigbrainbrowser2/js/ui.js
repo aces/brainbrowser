3d67771a251ec47528f7ee489be5f64d9401d8fc
Jonathan Lurie <lurie.jo@gmail.com>
2016-08-26 15:09:26 -0400

    add: almost done with creating the widgets

--- bigbrainbrowser2/js/ui.js
+++ bigbrainbrowser2/js/ui.js
@@ -13,6 +13,52 @@ $(function() {
 
 
 
+  $("#toggleAllShapes").click(function(){
+    $(".toggleShape").trigger("click");
+  });
+
+  /*
+
+  */
+  $(".toggleShape").click(function(){
+    toggleShape(this);
+  });
+
+
+
+
+  function toggleShape(toggleShapeButton){
+    //$("#leftSidebar").slideToggle("fast");
+    var slider = $(toggleShapeButton).siblings().find(".slider");
+    var eyeicon = $(toggleShapeButton).find(".eyeicon");
+    var sliderCurrentValue = $(slider).val();
+
+    // we want to switch off
+    if($(toggleShapeButton).attr("visible") == 1){
+      // saving the value
+      $(slider).attr("previousValue", sliderCurrentValue);
+      $(slider).val(0);
+      // disabling the slider
+      $(slider).prop('disabled', true);
+      $(slider).addClass("disabled");
+      $(toggleShapeButton).attr("visible", 0);
+      $(eyeicon).removeClass("fa-eye");
+      $(eyeicon).addClass("fa-eye-slash");
+      $(eyeicon).addClass("red");
+    }
+    // we want to switch on
+    else{
+      $(slider).prop('disabled', false);
+      $(slider).removeClass("disabled");
+      $(slider).val( $(slider).attr("previousValue") );
+      $(toggleShapeButton).attr("visible", 1);
+      $(eyeicon).removeClass("fa-eye-slash");
+      $(eyeicon).addClass("fa-eye")
+      $(eyeicon).removeClass("red");
+    }
+  }
+
+
 
 
   var availableTags = [
